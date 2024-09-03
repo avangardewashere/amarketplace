d9324b45eb53216816cafba8b7d3907e04ea99de
avan_web_okbet <avansoftify@gmail.com>
2024-09-03 18:29:24 +0800

    added icons

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,12 @@
 import { UserButton, useUser } from "@clerk/clerk-react"
 import { Button } from "../ui/button"
-
+import shopImg from "@/assets/shopping-bag.png"
 const Header = () => {
     const {user, isSignedIn} = useUser();
   return (
     <div>   <span>Home page</span>
-    <ul className="flex gap=1">
+    <ul className="flex  flex gap-1">
+        <li><img src={shopImg} alt="shop-image" className="w-12 h-12 " /></li>
        <li>Home</li>
        <li>Search</li>
        <li>New</li>
